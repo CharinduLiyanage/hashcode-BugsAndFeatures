e96d88ba74e460db5bd0d71cf6d34e201d45cd90
Charindu Liyanage <58912486+CharinduLiyanage@users.noreply.github.com>
2021-02-26 01:49:52 +0530

    input moduler

--- hashcode-Bugs&Features/src/TestingInput.java
+++ hashcode-Bugs&Features/src/TestingInput.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class TestingInput {
     public static void main(String[] args) {
         try {
-            InputFile inputFile = Main.readInputFile("Data/a.txt");
+            InputFile inputFile = InputFile.read("Data/a.txt");
             System.out.println(inputFile);
         } catch (IOException e) {
             e.printStackTrace();
